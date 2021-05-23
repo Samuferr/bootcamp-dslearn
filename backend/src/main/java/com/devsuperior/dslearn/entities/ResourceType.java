1c1448d4af1568b7df0737e30c842a7d52176f72
Samuferr <samuferr@gmail.com>
2021-05-23 19:10:25 -0300

    Resource

---
+++ backend/src/main/java/com/devsuperior/dslearn/entities/ResourceType.java
@@ -0,0 +1,5 @@
+package com.devsuperior.dslearn.entities;
+
+public enum ResourceType {
+	LESSON_ONLY, LESSON_TASK, FORUM, EXTERNAL_LINK;
+}
--- backend/src/main/java/com/devsuperior/dslearn/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/User.java
@@ -14,7 +14,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -33,58 +32,64 @@ public class User implements Serializable {
 		inverseJoinColumns = @JoinColumn(name = "role_id"))	
 	private Set<Role> roles = new HashSet<>();
 	
+	
 	public User() {
-		
 	}
 
-	public User(Long id, String name, String email, String password, Set<Role> roles) {
+
+	public User(Long id, String name, String email, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.roles = roles;
 	}
+
 
 	public Long getId() {
 		return id;
 	}
 
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 
 	public String getEmail() {
 		return email;
 	}
 
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 
 	public String getPassword() {
 		return password;
 	}
 
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 
 	public Set<Role> getRoles() {
 		return roles;
 	}
 
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
 
 	@Override
 	public int hashCode() {
@@ -93,6 +98,7 @@ public class User implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -110,4 +116,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+	
 }
